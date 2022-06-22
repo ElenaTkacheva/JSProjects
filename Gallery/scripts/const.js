78af87619ff4649a49aadc8cb956b5d412489cb1
Elena <79642915+ElenaTkacheva@users.noreply.github.com>
2022-06-22 11:15:34 -0400

    Add files via upload

---
+++ Gallery/scripts/const.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://api.unsplash.com';
+export const API_URL_PHOTOS = `${API_URL}/photos`;
+export const ACCESS_KEY = "R2uTX0xZCNHnc0DTBghe9mKHvtZEXroH40UkKES7EbE";
+export const SECRET_KEY = "dTEN7JCzLKDLqywE62i5qQ5s3B6H9xOXk91lhA7Vf-4";
--- Gallery/scripts/index.js
+++ Gallery/scripts/index.js
@@ -1,9 +1,24 @@
 import { getData } from "./getData.js";
 import { renderGallery } from "./renderGallery.js";
+import { renderPhoto } from "./renderPhoto.js";
 
-const init = async () => {
-    const photos = await getData();
-    renderGallery(photos);
+const init = async ({ selectorGalleryWrapper, selectorPhotoWrapper }) => {
+  const galleryWrapper = document.querySelector(selectorGalleryWrapper);
+  const photoWrapper = document.querySelector(selectorPhotoWrapper);
+
+    if(galleryWrapper) {
+        const photos = await getData({ count: 30 });
+        renderGallery(galleryWrapper, photos);
+    }
+    if(photoWrapper) {
+        const url = new URL(location.href);
+        const idPhoto = url.searchParams.get('photo');
+
+        if(idPhoto) {
+            const photo = await getData({ idPhoto });
+            renderPhoto(photoWrapper, photo);
+        }
+    }
 };
 
-init();
+init({ selectorGalleryWrapper: '.gallery__wrapper', selectorPhotoWrapper: '.photo__wrapper'});
