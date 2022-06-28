0566a07ab43d9d0985005b070b7ff9c06d2c394b
Elena <79642915+ElenaTkacheva@users.noreply.github.com>
2022-06-28 16:43:56 -0400

    Delete Gallery directory

--- Gallery/scripts/const.js
+++
@@ -1,9 +0,0 @@
-export const API_URL = 'https://api.unsplash.com';
-export const API_URL_PHOTOS = `${API_URL}/photos`;
-export const API_URL_TOKEN = "https://unsplash.com/oauth/token";
-export const API_URL_AUTH = "https://unsplash.com/oauth/authorize";
-export const ACCESS_KEY = "R2uTX0xZCNHnc0DTBghe9mKHvtZEXroH40UkKES7EbE";
-export const SECRET_KEY = "dTEN7JCzLKDLqywE62i5qQ5s3B6H9xOXk91lhA7Vf-4";
-export const REDIRECT_URI = 'http://127.0.0.1:5505/';
-export const RESPONSE_TYPE = 'code';
-export const SCOPE = 'public read_user read_photos write_likes';
--- Gallery/scripts/createElem.js
+++
@@ -1,6 +0,0 @@
-export const createElem = (tags, attrs) => {
-    const elem = document.createElement(tags);
-    Object.assign(elem, attrs);
-    return elem;
-};
-
